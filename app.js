102ee73625fc6e109e2b7777e409ee2279cf4477
Maximillian Lowe <maxjlowe@gmail.com>
2019-04-19 12:46:39 -0500

    Sec 7 Lec 78 complete

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // Initialize Weather Object
 const weather = new Weather('Boston', 'USA');
 
+//
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
 // weather.changeLocation('Barcelona', 'Spain'); will move to another location
 
 function getWeather(){
 weather.getWeather()
   .then(results => {
-    console.log(results);
+    ui.paint(results);
   })
   .catch(err => console.log(err));
 }
